27ba536dbe2ba932eca822876b68841c8bb8a414
ANL-007 <anicolaulourenco@ufl.edu>
2025-11-03 20:00:02 -0500

    Just making everything the same

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,4 @@
 print("Hello World")
 #User Input
 name = input("What is your name? ")
-print("Hello " + name + "! Welcome to the world of Python.")
+print("Hello " + name + "! Welcome to the world of Python!")
