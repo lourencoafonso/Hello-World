431b6b161c3693ca7e4e3fd92f3efd138b41939c
ANL-007 <anicolaulourenco@ufl.edu>
2025-11-03 19:52:06 -0500

    C++ and Python simple Hello World with user Input.

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //Display Hello World in terminal
+    cout << "Hello World!" << endl;
+    //Create string for user's name
+    string name;
+    //Input from User
+    cout << "What's your name? "; cin >> name;
+    //Display text in terminal
+    cout << "Hello " + name + "! Welcome to the world of c++!" << endl;
+    return 0;
+}
---
+++ HelloWorld.py
@@ -0,0 +1,4 @@
+print("Hello World")
+#User Input
+name = input("What is your name? ")
+print("Hello " + name + "! Welcome to the world of Python.")
